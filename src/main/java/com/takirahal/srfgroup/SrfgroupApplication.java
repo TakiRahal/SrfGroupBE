75a9f65fe4ed1c0692f12f3c6854a8dde20bbe21
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-02-14 08:46:18 +0100

    first commit

---
+++ src/main/java/com/takirahal/srfgroup/SrfgroupApplication.java
@@ -0,0 +1,13 @@
+package com.takirahal.srfgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrfgroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrfgroupApplication.class, args);
+    }
+
+}
