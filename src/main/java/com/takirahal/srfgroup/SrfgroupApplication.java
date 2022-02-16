061e4015da1f57d330a755dbcffdb69aaaf3239b
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-02-16 15:36:13 +0100

    Add Swagger

--- src/main/java/com/takirahal/srfgroup/SrfgroupApplication.java
+++ src/main/java/com/takirahal/srfgroup/SrfgroupApplication.java
@@ -6,15 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SrfgroupApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SrfgroupApplication.class, args);
-    }
-
-    @RequestMapping("/test")
-    public String welcome(){
-        return "Hello Boss";
     }
 }
---
+++ src/main/java/com/takirahal/srfgroup/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.takirahal.srfgroup.enums;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
