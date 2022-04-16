28c99cb06508a7daebbb0200942818c55f8f4155
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-04-16 15:04:44 +0100

    Multilanguage and push notifs

--- src/main/java/com/takirahal/srfgroup/modules/offer/services/impl/FindOfferServiceImpl.java
+++ src/main/java/com/takirahal/srfgroup/modules/offer/services/impl/FindOfferServiceImpl.java
@@ -2,6 +2,11 @@ package com.takirahal.srfgroup.modules.offer.services.impl;
 
 import com.takirahal.srfgroup.exceptions.BadRequestAlertException;
 import com.takirahal.srfgroup.exceptions.UnauthorizedException;
+import com.takirahal.srfgroup.modules.favoriteuser.entities.FavoriteUser;
+import com.takirahal.srfgroup.modules.favoriteuser.repositories.FavoriteUserRepository;
+import com.takirahal.srfgroup.modules.notification.dto.NotificationDTO;
+import com.takirahal.srfgroup.modules.notification.enums.ModuleNotification;
+import com.takirahal.srfgroup.modules.notification.services.NotificationService;
 import com.takirahal.srfgroup.modules.offer.dto.FindOfferDTO;
 import com.takirahal.srfgroup.modules.offer.dto.OfferImagesDTO;
 import com.takirahal.srfgroup.modules.offer.dto.RentOfferDTO;
@@ -13,25 +18,34 @@ import com.takirahal.srfgroup.modules.offer.repositories.FindOfferRepository;
 import com.takirahal.srfgroup.modules.offer.repositories.OfferImagesRepository;
 import com.takirahal.srfgroup.modules.offer.services.FindOfferService;
 import com.takirahal.srfgroup.modules.offer.services.OfferImagesService;
+import com.takirahal.srfgroup.modules.user.entities.UserOneSignal;
 import com.takirahal.srfgroup.modules.user.exceptioins.AccountResourceException;
 import com.takirahal.srfgroup.modules.user.mapper.UserMapper;
+import com.takirahal.srfgroup.modules.user.services.UserOneSignalService;
 import com.takirahal.srfgroup.security.UserPrincipal;
+import com.takirahal.srfgroup.services.impl.OneSignalService;
 import com.takirahal.srfgroup.services.impl.StorageService;
+import com.takirahal.srfgroup.utils.CommonUtil;
 import com.takirahal.srfgroup.utils.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.Predicate;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class FindOfferServiceImpl implements FindOfferService {
@@ -56,6 +70,21 @@ public class FindOfferServiceImpl implements FindOfferService {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    FavoriteUserRepository favoriteUserRepository;
+
+    @Autowired
+    NotificationService notificationService;
+
+    @Autowired
+    UserOneSignalService userOneSignalService;
+
+    @Autowired
+    OneSignalService oneSignalService;
+
+    @Autowired
+    MessageSource messageSource;
+
     @Override
     public FindOfferDTO save(FindOfferDTO findOfferDTO) {
         log.debug("Request to save FindOffer : {}", findOfferDTO);
@@ -76,6 +105,9 @@ public class FindOfferServiceImpl implements FindOfferService {
             offerImagesDTO.setOffer(result);
             offerImagesService.save(offerImagesDTO);
         }
+
+        // Create Notification and send push notification for favorite users
+        createNotificationsForFavoriteUsers();
 
         return result;
     }
@@ -131,6 +163,33 @@ public class FindOfferServiceImpl implements FindOfferService {
         }
 
         return newFindSellOfferDTO;
+    }
+
+    @Async
+    public void createNotificationsForFavoriteUsers(){
+        List<FavoriteUser> favoriteUserList = favoriteUserRepository.findYourFavoriteUsers();
+        for ( FavoriteUser favoriteUser: favoriteUserList ) {
+
+            Locale locale = Locale.forLanguageTag(!favoriteUser.getCurrentUser().getLangKey().equals("") ? favoriteUser.getCurrentUser().getLangKey() : "fr");
+            String messageCommentOffer = CommonUtil.getFullNameUser(userMapper.toDto(favoriteUser.getFavoriteUser()))+" "+messageSource.getMessage("offer.message_favorite_user_add_new_offer", null, locale);
+
+            // Create notification
+            NotificationDTO notificationDTO = new NotificationDTO();
+            notificationDTO.setDateCreated(Instant.now());
+            notificationDTO.setContent(messageCommentOffer);
+            notificationDTO.setModule(ModuleNotification.OfferNotification.name());
+            notificationDTO.setIsRead(Boolean.FALSE);
+            notificationDTO.setUser(userMapper.toDto(favoriteUser.getCurrentUser()));
+            notificationService.save(notificationDTO);
+
+            // Send Push Notif
+            List<UserOneSignal> listUserOneSignals = userOneSignalService.findByUser(favoriteUser.getCurrentUser());
+            if(listUserOneSignals.size()>0){
+                String result = listUserOneSignals.stream().map(UserOneSignal::getIdOneSignal)
+                        .collect(Collectors.joining(","));
+                oneSignalService.sendPushNotifToUser(result, messageCommentOffer);
+            }
+        }
     }
 
     private Specification<FindOffer> createSpecification(FindOfferFilter findOfferFilter) {
--- src/main/java/com/takirahal/srfgroup/modules/offer/services/impl/RentOfferServiceImpl.java
+++ src/main/java/com/takirahal/srfgroup/modules/offer/services/impl/RentOfferServiceImpl.java
@@ -2,6 +2,11 @@ package com.takirahal.srfgroup.modules.offer.services.impl;
 
 import com.takirahal.srfgroup.exceptions.BadRequestAlertException;
 import com.takirahal.srfgroup.exceptions.UnauthorizedException;
+import com.takirahal.srfgroup.modules.favoriteuser.entities.FavoriteUser;
+import com.takirahal.srfgroup.modules.favoriteuser.repositories.FavoriteUserRepository;
+import com.takirahal.srfgroup.modules.notification.dto.NotificationDTO;
+import com.takirahal.srfgroup.modules.notification.enums.ModuleNotification;
+import com.takirahal.srfgroup.modules.notification.services.NotificationService;
 import com.takirahal.srfgroup.modules.offer.dto.OfferImagesDTO;
 import com.takirahal.srfgroup.modules.offer.dto.RentOfferDTO;
 import com.takirahal.srfgroup.modules.offer.dto.SellOfferDTO;
@@ -13,25 +18,34 @@ import com.takirahal.srfgroup.modules.offer.repositories.OfferImagesRepository;
 import com.takirahal.srfgroup.modules.offer.repositories.RentOfferRepository;
 import com.takirahal.srfgroup.modules.offer.services.OfferImagesService;
 import com.takirahal.srfgroup.modules.offer.services.RentOfferService;
+import com.takirahal.srfgroup.modules.user.entities.UserOneSignal;
 import com.takirahal.srfgroup.modules.user.exceptioins.AccountResourceException;
 import com.takirahal.srfgroup.modules.user.mapper.UserMapper;
+import com.takirahal.srfgroup.modules.user.services.UserOneSignalService;
 import com.takirahal.srfgroup.security.UserPrincipal;
+import com.takirahal.srfgroup.services.impl.OneSignalService;
 import com.takirahal.srfgroup.services.impl.StorageService;
+import com.takirahal.srfgroup.utils.CommonUtil;
 import com.takirahal.srfgroup.utils.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.Predicate;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class RentOfferServiceImpl implements RentOfferService {
@@ -56,6 +70,21 @@ public class RentOfferServiceImpl implements RentOfferService {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    FavoriteUserRepository favoriteUserRepository;
+
+    @Autowired
+    NotificationService notificationService;
+
+    @Autowired
+    UserOneSignalService userOneSignalService;
+
+    @Autowired
+    OneSignalService oneSignalService;
+
+    @Autowired
+    MessageSource messageSource;
+
     @Override
     public RentOfferDTO save(RentOfferDTO rentOfferDTO) {
         log.debug("Request to save RentOffer : {}", rentOfferDTO);
@@ -75,6 +104,9 @@ public class RentOfferServiceImpl implements RentOfferService {
             offerImagesDTO.setOffer(result);
             offerImagesService.save(offerImagesDTO);
         }
+
+        // Create Notification and send push notification for favorite users
+        createNotificationsForFavoriteUsers();
 
         return result;
     }
@@ -130,6 +162,33 @@ public class RentOfferServiceImpl implements RentOfferService {
         }
 
         return newSellOfferDTO;
+    }
+
+    @Async
+    public void createNotificationsForFavoriteUsers(){
+        List<FavoriteUser> favoriteUserList = favoriteUserRepository.findYourFavoriteUsers();
+        for ( FavoriteUser favoriteUser: favoriteUserList ) {
+
+            Locale locale = Locale.forLanguageTag(!favoriteUser.getCurrentUser().getLangKey().equals("") ? favoriteUser.getCurrentUser().getLangKey() : "fr");
+            String messageCommentOffer = CommonUtil.getFullNameUser(userMapper.toDto(favoriteUser.getFavoriteUser()))+" "+messageSource.getMessage("offer.message_favorite_user_add_new_offer", null, locale);
+
+            // Create notification
+            NotificationDTO notificationDTO = new NotificationDTO();
+            notificationDTO.setDateCreated(Instant.now());
+            notificationDTO.setContent(messageCommentOffer);
+            notificationDTO.setModule(ModuleNotification.OfferNotification.name());
+            notificationDTO.setIsRead(Boolean.FALSE);
+            notificationDTO.setUser(userMapper.toDto(favoriteUser.getCurrentUser()));
+            notificationService.save(notificationDTO);
+
+            // Send Push Notif
+            List<UserOneSignal> listUserOneSignals = userOneSignalService.findByUser(favoriteUser.getCurrentUser());
+            if(listUserOneSignals.size()>0){
+                String result = listUserOneSignals.stream().map(UserOneSignal::getIdOneSignal)
+                        .collect(Collectors.joining(","));
+                oneSignalService.sendPushNotifToUser(result, messageCommentOffer);
+            }
+        }
     }
 
     private Specification<RentOffer> createSpecification(RentOfferFilter rentOfferFilter) {
